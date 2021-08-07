f8c8baba41c527532a37284c939a9b58efce6eba
Janos Vajda <vajdajanos@gmail.com>
2021-08-07 12:37:05 +0100

    User database schema has been created.

---
+++ user/src/schemas/user-link.schema.ts
@@ -0,0 +1,35 @@
+import * as mongoose from 'mongoose';
+
+function transformValue(doc, ret: { [key: string]: any }) {
+  delete ret._id;
+}
+
+function generateLink() {
+  return Math.random().toString(36).replace('0.', '');
+}
+
+export const UserLinkSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: String,
+    },
+    is_used: {
+      type: Boolean
+    },
+    link: {
+      type: String
+    },
+  },
+  {
+    toObject: {
+      virtuals: true,
+      versionKey: false,
+      transform: transformValue,
+    },
+    toJSON: {
+      virtuals: true,
+      versionKey: false,
+      transform: transformValue,
+    },
+  },
+);
---
+++ user/src/schemas/user.schema.ts
@@ -0,0 +1,44 @@
+import * as mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { IUser } from '../interfaces/user.interface';
+
+const SALT_ROUNDS = 10;
+
+function transformValue(doc, ret: { [key: string]: any }) {
+  delete ret._id;
+  delete ret.password;
+}
+
+export interface IUserSchema extends mongoose.Document {
+  email: string;
+  password: string;
+  is_confirmed: boolean;
+  comparePassword: (password: string) => Promise<Boolean>
+  getEncryptedPassword: (password: string) => Promise<string>;
+};
+
+export const UserSchema = new mongoose.Schema<IUserSchema>(
+  {
+    email: {
+      type: String
+    },
+    is_confirmed: {
+      type: Boolean
+    },
+    password: {
+      type: String
+    },
+  },
+  {
+    toObject: {
+      virtuals: true,
+      versionKey: false,
+      transform: transformValue,
+    },
+    toJSON: {
+      virtuals: true,
+      versionKey: false,
+      transform: transformValue,
+    },
+  },
+);
