eb23065102ce9a916d8cfbfd0b965225eddcbc60
Janos Vajda <vajdajanos@gmail.com>
2021-08-07 13:35:18 +0100

    Permission microservice controller has been created.

--- permission/src/main.ts
+++ permission/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Transport, TcpOptions } from '@nestjs/microservices';
+
+import { PermissionModule } from './permission.module';
+import { ConfigService } from './services/config/config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(PermissionModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: new ConfigService().get('port'),
+    },
+  } as TcpOptions);
+  await app.listenAsync();
 }
 bootstrap();
